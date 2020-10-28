fd050ca7fb8fdfb69cc4099670254356cafb4a62
Andrea Galvani <17992288+IlGalvo@users.noreply.github.com>
2020-10-28 14:11:54 +0100

    Small improvements.

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -3,66 +3,72 @@ import os
 import re
 from collections import OrderedDict
 
+os.chdir(os.path.dirname(__file__))
+
 sys.argv.append("out2.lp")
+sys.argv.append("parsed_out2.lp")
 
 print("Number of arguments:", len(sys.argv), "arguments.")
-print("Argument List:", sys.argv)
+print("Arguments List:", sys.argv, "\n")
 
 if sys.argv.count == 1:
-    print("no args")
+    print("No args. Exiting...")
     exit()
 
 if not os.path.isfile(sys.argv[1]):
-    print("no file")
+    print("No file. Exiting...")
     exit()
 
 with open(sys.argv[1], "r") as in_file:
     file_content = in_file.read()
 
-# print(file_content)
-rgx1 = r"Answer: -?\d+"
-rgx2 = r"Optimization: -?\d+"
-rgx3 = r"-?\d+"
+answer_regex = r"Answer: -?\d+"
+optimization_regex = r"Optimization: -?\d+"
+number_regex = r"-?\d+"
 
-value1=0
-value2=""
+optimization_value = 0
+content=""
 
 while True:
-    start_regex = re.search(rgx1, file_content)
+    start_regex = re.search(answer_regex, file_content)
 
     if not start_regex:
         break
 
-    start_regex=start_regex.group(0)
+    start_regex = start_regex.group(0)
     start_index = file_content.index(start_regex) + len(start_regex)
 
-    end_regex = re.search(rgx2, file_content).group(0)
+    end_regex = re.search(optimization_regex, file_content).group(0)
     end_index = file_content.index(end_regex)
 
-    value_regex = int(re.search(rgx3, end_regex).group(0))
+    tmp_optimization_value = int(re.search(number_regex, end_regex).group(0))
 
-    if int(value_regex) <= value1:
-        value1=value_regex
-        value2=file_content[start_index:end_index]
+    if tmp_optimization_value <= optimization_value:
+        optimization_value = tmp_optimization_value
+        content = file_content[start_index : end_index]
     
-    file_content = file_content[end_index+len(end_regex)+1:]
+    file_content = file_content[end_index + len(end_regex) + 1 :]
 
-print(value1, value2)
+print("Optimization value:", optimization_value)
 
-values = value2.replace("\n", "").split(") ")
-print(values)
+content_values = content.replace("\n", "").split(") ")
+print("Content values:", content_values, "\n")
 
-dictionary={}
+dictionary = {}
 
-for value in values:
-    info = value.split(",")
+for content_value in content_values:
+    values = content_value.split(",")
 
-    id = int(re.search(rgx3, info[0]).group(0))
-    name = info[1] 
+    id = int(re.search(number_regex, values[0]).group(0))
+    name = values[1] 
     
     dictionary[id] = name
 
 dictionary = OrderedDict(sorted(dictionary.items()))
 
-for entry in dictionary:
-    print("Found artery name:", dictionary[entry],"with id:", entry)
+with open(sys.argv[2], "w") as out_file:
+    for entry in dictionary:
+        output = "Found artery with id: " + str(entry) + " and name: " + dictionary[entry] + "."
+        print(output)
+
+        out_file.write(output + "\n")
