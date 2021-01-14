e6846cd9756761a353c15f8b1e2ee3be9c81ca33
Andrea Galvani <17992288+IlGalvo@users.noreply.github.com>
2021-01-14 21:03:18 +0100

    New specification's first concept.

--- src/parser.py
+++ src/parser.py
@@ -538,43 +538,323 @@ def write_arteries_graph(file_name: str, arteries: list, is_debug: bool):
     dot.render(file_name, view=is_debug)
 
 
+# Edge rule referes to two arteries IDs
+class Edge2(IRule):
+    def __init__(self, artery1: str, artery2: str):
+        self._artery1 = artery1
+        self._artery2 = artery2
+
+    def to_text(self) -> str:
+        return self.__str__()
+
+    def __str__(self) -> str:
+        return self._artery1 + " is connected to " + self._artery2 + "."
+
+
+# Heigth rule referes to two arteries Z and their offsets
+class Heigth2(IRule):
+    def __init__(self, heigth_type: HeigthType,
+                 artery1: str, offset1: str,
+                 artery2: str, offset2: str):
+        self._heigth_type = heigth_type
+
+        self._artery1 = artery1
+        self._offset1 = offset1
+
+        self._artery2 = artery2
+        self._offset2 = offset2
+
+    def to_text(self) -> str:
+        return self.__str__()
+
+    def __str__(self) -> str:
+        text = self._artery1 + " heigth" + self._offset1 + " is "
+
+        text += "greater" if self._heigth_type == HeigthType.Greater else "less"
+
+        return text + " than " + self._artery2 + " heigth" + self._offset2 + "."
+
+
+# General rule has a text description and can refer to an artery
+class General2(IRule):
+    def __init__(self, rule_text: str, artery: str = None):
+        self._rule_text = rule_text
+        self._artery = artery
+
+    def to_text(self) -> str:
+        return self.__str__()
+
+    def __str__(self) -> str:
+        if self._artery != None:
+            return self._artery + " has " + self._rule_text + "."
+
+        return self._rule_text
+
+
+class Model:
+    def __init__(self, id: int, variant: int, edge: Edge2):
+        self._id = id
+        self._variant = variant
+
+        self._edge = edge
+
+    def get_id(self) -> int:
+        return self._id
+
+    def get_variant(self) -> int:
+        return self._variant
+
+    def get_artery1(self) -> str:
+        return self._artery1
+
+    def get_artery2(self) -> str:
+        return self._artery2
+
+    def __str__(self) -> str:
+        text = "Model with ID: [" + str(self._id) + \
+            "] and Variant: [" + str(self._variant) + "].\n"
+
+        return text + "\tRule: " + self._edge.to_text() + "\n"
+
+
+# OutputArtery has and id and name,
+# and contains a list of ConfidenceRule
+class OutputArtery2:
+    def __init__(self, id: int, name: str):
+        self._id = id
+        self._name = name
+
+        self._confidence_rules = []
+
+    def get_id(self) -> int:
+        return self._id
+
+    def get_name(self) -> str:
+        return self._name
+
+    def get_confidence_rules(self) -> list:
+        return self._confidence_rules
+
+    def __str__(self) -> str:
+        text = "Artery with ID: [" + \
+            str(self._id) + "] and Name: [" + self._name + "].\n"
+
+        for confidence_rule in self._confidence_rules:
+            text += "\t" + str(confidence_rule) + "\n"
+
+        return (text + "\n")
+
+
+def parse_arteries_classifier2(file_name: str):
+    with open(file_name, "r") as in_file:
+        lines = in_file.readlines()
+
+    models = []
+    confidence_rules = []
+
+    for line in lines:
+        if line.startswith("model") and "ID" not in line:
+            info = line.replace("\n", "").replace(
+                "(", ",").replace(")", ",").split(",")
+
+            id = int(info[1])
+            variant = int(info[2])
+            edge = Edge2(info[3], info[4])
+
+            models.append(Model(id, variant, edge))
+        elif line.startswith("confidence_rule") and "anatomy" not in line:
+            # [0] = confidence rule info, [1...N] = rules
+            info = line.replace("\n", "").split(", ")
+
+            # Extract confidence rule id and name
+            cr_text = info[0].split("= ")
+
+            id = int(number_regex1.search(cr_text[0]).group(0))
+            name = cr_text[1]
+
+            confidence_rule = ConfidenceRule(id, name)
+
+            # Temp
+            arteries = []
+
+            # Skip [0] because it's confidence rule info
+            for i in range(1, len(info)):
+                # [0] = rule name, [1...N] = args
+                rule = info[i].replace("(", ",").replace(")", ",").split(",")
+
+                # artery(ID,R,D,Q,X,Y,Z,PL,DE,H,A,N)
+                if rule[0] == "artery":
+                    # If args are not equal to "_", they're valid
+                    # If name is equal to "N", it refers to confidence rule principal artery
+                    # If name is equal to "N", it refers to confidence rule name
+                    id = rule[1] if rule[1] != "_" else None
+                    radius = rule[2] if rule[2] != "_" else None
+                    density = rule[3] if rule[3] != "_" else None
+                    quality = rule[4] if rule[4] != "_" else None
+                    cog_x = rule[5] if rule[5] != "_" else None
+                    cog_y = rule[6] if rule[6] != "_" else None
+                    cog_z = rule[7] if rule[7] != "_" else None
+                    path_length = rule[8] if rule[8] != "_" else None
+                    distance_from_extremes = rule[9] if rule[9] != "_" else None
+                    heigth = rule[10] if rule[10] != "_" else None
+                    angle = rule[11] if rule[11] != "_" else None
+
+                    is_primary = rule[12] == "N"
+                    name = rule[12] if rule[12] != "N" else confidence_rule.get_name()
+
+                    artery = ParamsArtery(is_primary, name,
+                                          id, radius, density, quality,
+                                          cog_x, cog_y, cog_z,
+                                          path_length, distance_from_extremes,
+                                          heigth, angle)
+                    arteries.append(artery)
+
+                # edge(ID1,ID2) or edge(name,ID)
+                elif rule[0] == "edge":
+                    # Find arteries for ID
+                    artery1 = next((artery.get_name() for artery in arteries
+                                    if artery.get_id() == rule[1]), rule[1])
+                    artery2 = next(artery.get_name() for artery in arteries
+                                   if artery.get_id() == rule[2])
+
+                    edge_rule = Edge2(artery1, artery2)
+                    confidence_rule.get_rules().append(edge_rule)
+
+                # height_greater/height_less(Z1+X1,Z2+X2), with +X1 and +X2 optional
+                elif rule[0] == "height_greater" or rule[0] == "height_less":
+                    heigth_type = HeigthType.Greater if rule[0] == "height_greater" else HeigthType.Less
+
+                    # Check if there are +X1 and +X2
+                    offset_regex1 = number_regex2.search(rule[1])
+                    offset_regex2 = number_regex2.search(rule[2])
+
+                    # If success, get the value
+                    offset1 = offset_regex1.group(
+                        0) if offset_regex1 != None else ""
+                    offset2 = offset_regex2.group(
+                        0) if offset_regex2 != None else ""
+
+                    # Substring to get the Z
+                    heigth1 = rule[1][0: offset_regex1.start(
+                    )] if offset_regex1 != None else rule[1]
+                    heigth2 = rule[1][0: offset_regex2.start(
+                    )] if offset_regex2 != None else rule[2]
+
+                    # Find arteries for Z
+                    artery1 = next(artery.get_name() for artery in arteries
+                                   if artery.get_heigth() == heigth1)
+                    artery2 = next(artery.get_name() for artery in arteries
+                                   if artery.get_heigth() == heigth2)
+
+                    heigth_rule = Heigth2(heigth_type,
+                                          artery1, offset1,
+                                          artery2, offset2)
+                    confidence_rule.get_rules().append(heigth_rule)
+
+                # It's a self meaning rule
+                else:
+                    # Check if it's a general rule contained in general rule dictionary
+                    rule_text = general_rule_dictionary.get(rule[0], None)
+
+                    if rule_text != None:
+                        # Find primary artery
+                        artery1 = next(artery.get_name() for artery in arteries
+                                       if artery.get_is_primary)
+
+                        general_rule = General2(rule_text, artery1)
+                        confidence_rule.get_rules().append(general_rule)
+                    else:
+                        # Parse as generic text rule
+                        info[0] = confidence_rule.get_name() + " has: "
+                        rule_text = " ".join(info)
+
+                        general_rule = General2(rule_text)
+                        confidence_rule.get_rules().append(general_rule)
+
+                        break
+
+            confidence_rules.append(confidence_rule)
+
+    return models, confidence_rules
+
+
+# Parses in_artery_classified.lp file with confidence rules
+# and returns a list of all arteries found
+def parse_artery_classified2(file_name: str, models: list, confidence_rules: list) -> list:
+    models2 = []
+    arteries = []
+    dot = Digraph(comment='Arteries')
+
+    with open(file_name, "r") as in_file:
+        file_content = in_file.read()
+
+    answers = file_content.split("Answer: ")
+
+    # Support variable to find best answer
+    facts_text = ""
+    optimization_value = 0
+
+    # Skip [0] because of ASP verbose info
+    for i in range(1, len(answers)):
+        # [0] = answer number (skip), [1] = facts, [2] = optimizazion_value
+        answer = answers[i].splitlines()
+
+        tmp_opt_value = number_regex1.search(answer[2]).group(0)
+        tmp_opt_value = int(tmp_opt_value)
+
+        # Lower is best
+        if tmp_opt_value <= optimization_value:
+            optimization_value = tmp_opt_value
+            facts_text = answer[1]
+
+    # Can be: artery(id,name) or confidence_rule(name,id)
+    for facts in facts_text.split(' '):
+        fact = facts.replace("(", ",").replace(")", "").split(",")
+
+        if fact[0] == "model_gen1":
+            id = int(fact[1])
+            variant = int(fact[2])
+
+            model = next(model for model in models
+                         if model.get_id() == id and
+                         model.get_variant() == variant)
+
+            models2.append(model)
+        elif fact[0] == "out_artery1":
+            id = int(fact[1])
+            name = fact[2]
+
+            artery = OutputArtery2(id, name)
+            arteries.append(artery)
+        elif fact[0] == "cleaned_cr1":
+            id = int(fact[2])
+            name = fact[1]
+
+            # Find associated confidence rule for id and name
+            confidence_rule = next(confidence_rule for confidence_rule in confidence_rules
+                                   if confidence_rule.get_id() == id and
+                                   confidence_rule.get_name() == name)
+
+            # Find artery associated to confidence rule for name
+            artery = next(artery for artery in arteries
+                          if artery.get_name() == name)
+
+            artery.get_confidence_rules().append(confidence_rule)
+        elif fact[0] == "edge_out":
+            artery1 = fact[1]
+            artery2 = fact[2]
+
+            dot.edge(artery1, artery2)
+
+    # Sort for artery id
+    arteries.sort(key=lambda artery: artery.get_id())
+
+    return models2, arteries, dot
+
+
 def main():
     # To simplify debug
     is_debug = True
-
-    dot = Digraph(comment='tmp')
-
-    dot.node("1", "artery1")
-    dot.node("1_1", "rule1")
-    dot.node("1_2", "rule2")
-    dot.node("1_3", "rule3")
-
-    dot.node("2", "artery2")
-    dot.node("2_1", "rule1")
-    dot.node("2_2", "rule2")
-
-    dot.node("3", "artery3")
-    dot.node("3_1", "rule1")
-
-    dot.node("4", "artery4")
-
-    dot.edge("1", "1_1")
-    dot.edge("1", "1_2")
-    dot.edge("1", "1_3")
-
-    dot.edge("2", "2_1")
-    dot.edge("2", "2_2")
-
-    dot.edge("3", "3_1")
-
-    dot.edge("1", "2")
-    dot.edge("1", "3")
-    dot.edge("3", "4")
-
-    dot.edge("2_2", "4")
-
-    dot.render("tmptmtmp.svg", view=is_debug)
-    exit()
 
     if is_debug:
         os.chdir(os.path.dirname(__file__))
@@ -585,6 +865,25 @@ def main():
     elif len(sys.argv) < 3 or not os.path.isfile(sys.argv[1]) or not os.path.isfile(sys.argv[2]):
         print("Usage: python parser.py in_arteries_classifier.lp in_arteries_classified.lp out_arteries_parsed.lp")
         exit()
+
+    #x, y = parse_arteries_classifier2(sys.argv[1])
+    x = []
+    y = []
+
+    for model in x:
+        print(model)
+    for confidence_rule in y:
+        print(confidence_rule)
+
+    x, y, z = parse_artery_classified2(sys.argv[2], x, y)
+
+    for model in x:
+        print(model)
+    for artery in y:
+        print(artery)
+    z.render("Arteries.svg", view=is_debug)
+
+    exit()
 
     arteries = parse_arteries_classifier(sys.argv[1])
 
